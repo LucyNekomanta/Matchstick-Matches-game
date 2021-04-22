5d73d3e17ff53e5390eb26842d180e33a256c9cf
LucyNekomanta <dennis.nicolai@outlook.com>
2021-04-22 16:24:36 +0200

    Refactoring HumanGame, switch who goes first now possible

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,117 +8,90 @@ namespace _7_Aufgabe_7__NIMM_DREI__Dennis_Nicolai
 {
     class Program
     {
-        static int numCheckOutput, dynamicNumCheck;
         static int MatchsticksNumber;
-        static bool goFirst, enableLimitlessNumCheck;
 
-        //static string coinToss;
-
-        static void CoinTossFunction()
+        static bool CoinTossFunction()
         {
             Console.WriteLine("Now we toss a coin to decide who goes first, choose: Heads(1), or Tails(2) ?{0}" +
                 "Type 1 for Heads(1) or 2 for Tails(2)",
                     Environment.NewLine);
             int rndValue, schroedinger;
-            enableLimitlessNumCheck = false;
-            dynamicNumCheck = 2;
-            NumCheckFunction();
-            schroedinger = numCheckOutput;
+            schroedinger = NumCheckFunction(2);
             Random rnd = new Random();
             rndValue = rnd.Next(1, 2);
             if (rndValue == schroedinger)
             {
-                goFirst = true;
+                //goFirst = true;
                 Console.WriteLine("Player1 guessed correct, he goes first.");
+                return true;
             }
             else
             {
-                goFirst = false;
+                //goFirst = false;
                 Console.WriteLine("Player1 guessed incorrect, he goes second.");
+                return false;
             }
         }
-        static void NumCheckFunction()
+        static int NumCheckFunction(int dynamicNumCheck)
         //checks if input is a number that is bigger than 0 or in range
         {
-            bool numCheck;
-            numCheck = false;
+            bool numCheck=false;
+            int numCheckOutput;
 
-            if (!enableLimitlessNumCheck)
-            {
                 while (!numCheck)
                 {
                     string input = Console.ReadLine();
                     if (int.TryParse(input, out numCheckOutput) && numCheckOutput > 0 && numCheckOutput <= dynamicNumCheck)
                     {
                         numCheck = true;
+                        return numCheckOutput;
                     }
                     else
                     {
                         Console.WriteLine("You have to write a number that is in the range of 1 and " + dynamicNumCheck);
                     }
                 }
-            }
-            else
-            {
-                while (!numCheck)
-                {
-                    string input = Console.ReadLine();
-                    if (int.TryParse(input, out numCheckOutput) && numCheckOutput > 0)
-                    {
-                        numCheck = true;
-                        enableLimitlessNumCheck = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("You have to write a number that is bigger than zero.");
-                    }
-                }
-            }
+                return 0;
         }
-        static void HumanGame()
+        static void HumanGame(bool goFirst)
         //human vs human
         {
-            int player1, player2;
-            enableLimitlessNumCheck = false;
-            dynamicNumCheck = 3;
+            int drawYourLastPatheticCard;
+            bool turnPlayer1=goFirst;
 
             while (MatchsticksNumber > 0)
             {
                 ShowMatchsticksFunction();
-                Console.Write("Player1, choose a number from 1 to 3 to take: ");
-                NumCheckFunction();
-                player1 = numCheckOutput;
+                
 
-                MatchsticksNumber = MatchsticksNumber - player1;
+                if (turnPlayer1)
+    {
+                    Console.Write("Player1, choose a number from 1 to 3 to take: ");
+                    //turnPlayer1=false;
+    }
+                else
+    {
+                    Console.Write("Player2, choose a number from 1 to 3 to take: ");
+                    //turnPlayer1=true;
+    }
+                drawYourLastPatheticCard = NumCheckFunction(3);
+
+                MatchsticksNumber = MatchsticksNumber - drawYourLastPatheticCard;
 
                 if (MatchsticksNumber <= 0)
 
                 {
-
-                    Console.WriteLine("Player1 lost, Player2 wins the game");
-
-                }
-
-                else
-
-                {
-                    ShowMatchsticksFunction();
-                    Console.Write("Player2, choose a number from 1 to 3 to take: ");
-                    NumCheckFunction();
-                    player2 = numCheckOutput;
-                    MatchsticksNumber = MatchsticksNumber - player2;
-
-
-                    if (MatchsticksNumber <= 0)
-
-                    {
-
+                    if (turnPlayer1)
+    {
+                        Console.WriteLine("Player1 lost, Player2 wins the game");
+    }
+                    else
+    {
                         Console.WriteLine("Player2 lost, Player1 wins the game");
-
-                    }
-
+    }
+                    
                 }
-
+                turnPlayer1=!turnPlayer1;
             }
         }
         static void PlayerSelect()
@@ -126,11 +99,8 @@ namespace _7_Aufgabe_7__NIMM_DREI__Dennis_Nicolai
             Console.WriteLine("Do you want to play against an AI player(1) or against a human player(2)(who has to sit next to you)?{0}" +
                 "Write '1' for AI player or '2' for human player.",
                     Environment.NewLine);
-            enableLimitlessNumCheck = false;
-            dynamicNumCheck = 2;
-            NumCheckFunction();
-            int helper = numCheckOutput;
-            CoinTossFunction();
+            int helper = NumCheckFunction(2);
+            bool goFirst = CoinTossFunction();
             if (helper == 2)
             {
                 if (!goFirst)
@@ -141,7 +111,7 @@ namespace _7_Aufgabe_7__NIMM_DREI__Dennis_Nicolai
                 {
                     Console.WriteLine("Player1 goes first");
                 }
-                HumanGame();
+                HumanGame(goFirst);
             }
             else
             {
@@ -153,12 +123,12 @@ namespace _7_Aufgabe_7__NIMM_DREI__Dennis_Nicolai
                 {
                     Console.WriteLine("AI goes second");
                 }
-                AIgame();
+                AIgame(goFirst);
             }
 
 
         }
-        static void AIgame()
+        static void AIgame(bool goFirst)
 
         {
 
@@ -171,11 +141,7 @@ namespace _7_Aufgabe_7__NIMM_DREI__Dennis_Nicolai
             {
                 ShowMatchsticksFunction();
                 Console.Write("Player1, choose a number from 1 to 3 to take: ");
-                enableLimitlessNumCheck=false;
-                dynamicNumCheck=3;
-                NumCheckFunction();
-                player1=numCheckOutput;
-
+                player1=NumCheckFunction(3);
                 MatchsticksNumber = MatchsticksNumber - player1;
 
                 if (MatchsticksNumber <= 0)
@@ -223,9 +189,7 @@ namespace _7_Aufgabe_7__NIMM_DREI__Dennis_Nicolai
                 "Whichever player takes the last stick looses the game.{0}" +
                 "Please enter the Number of Matchsticks used: ",
                     Environment.NewLine);
-            enableLimitlessNumCheck = true;
-            NumCheckFunction();
-            MatchsticksNumber = numCheckOutput;
+            MatchsticksNumber = NumCheckFunction(1000);
             PlayerSelect();
 
             Console.WriteLine("Press any key to end");
